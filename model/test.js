4fadf670eeff3468da32e936388a2bdcf6769d49
Vinoth Kumar <vinoct6@gmail.com>
2015-12-19 12:28:35 +0530

    Inheritance in Backbone.Model

--- model/test.js
+++ model/test.js
@@ -27,3 +27,41 @@ console.log (v2.prop1); //v2's property remains the same
 
 //Class property is like static prperty in Java. They become available directly on the types
 console.log (Vehicle.summary());
+
+//---- inheritence---
+
+//Models can inherit from other models
+
+var Car = Vehicle.extend({});
+
+var A = Backbone.Model.extend({
+  initialize : function () {
+    console.log ("Init object type A");
+  },
+
+  asString : function () {
+      return JSON.stringify(this.toJSON());
+  }
+
+
+});
+
+var a = new A({
+    one : '1',
+    two : '2'
+});
+
+console.log(a.asString());
+
+var B = A.extend({});
+
+var b = new B ({
+  three : '3',
+  four :'4'
+}
+);
+
+console.log(b.asString());
+
+console.log (b instanceof A); //true
+console.log (b instanceof Backbone.Model);
